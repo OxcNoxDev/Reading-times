3e8a5f5d10f29fffb3f88c18be76a87fdc5bfda3
OxcNoxDev <118824350+Zhilshi04@users.noreply.github.com>
2023-06-01 22:15:40 +0700

    updata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <div className="header" style={{background : 'red',height:'80px',display:'flex',justifyContent:'space-between',alignItems:'center'}}>
+          <div className="time" style={{color:'white',margin:'0px 20px 0px 20px',fontSize:'30px'}}>Time</div>
+          <div className="disboard" style={{color:'white',margin:'0px 20px 0px 20px',fontSize:'30px'}}>Disboard</div>
+        </div>
         <Main />
         <NextScript />
       </body>
---
+++ src/pages/disboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function disboard({}: Props) {
+  return (
+    <div>disboard</div>
+  )
+}
